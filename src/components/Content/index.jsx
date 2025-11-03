bf2d17b1f90d9a7f38aba2cc9dee7c0faa168298
Mariana <mariana.iwaivk@gmail.com>
2025-11-03 01:26:51 -0300

    feat(content): add Content component with Hero, Products, About and Contact sections

---
+++ src/components/Content/index.jsx
@@ -0,0 +1,15 @@
+import HeroSection from '../HeroSection';
+import ProductsSection from '../ProductsSection';
+import AboutSection from '../AboutSection';
+import ContactSection from '../ContactSection';
+
+export default function Content() {
+  return (
+    <main>
+      <HeroSection />
+      <ProductsSection />
+      <AboutSection />
+      <ContactSection />
+    </main>
+  );
+}
