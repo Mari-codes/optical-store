721191be8e35a92059dd57e89cbf9ce3851cb218
Mariana <mariana.iwaivk@gmail.com>
2025-11-03 01:31:08 -0300

    feat(home-page): add index page with Content component

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,13 @@
 import "@/styles/globals.css";
+import Header from "@/components/Header";  // Header/index.jsx
+import Footer from "@/components/Footer";  // Footer/index.jsx
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Header />
+      <Component {...pageProps} />
+      <Footer />
+    </>
+  );
 }
